694ca96d22be77d816e9f4c4315cf223654f2f00
JMCalizario <35472547+JMCalizario@users.noreply.github.com>
2020-10-13 19:57:48 -0300

    Add files via upload

---
+++ NumeradorDenominador.py
@@ -0,0 +1,19 @@
+a = int(input('Digite o Numerador '))
+b = int(input('Digite o Denominador '))
+print('{}'.format(a))
+print('--')
+print('{}'.format(b))
+
+
+if  a >  b:
+     maiorQ = a
+else:
+   maiorQ = b
+   while True :
+       if maiorQ  % a == 0 and maiorQ % b == 0:
+           print('mmc de {} e {} é {}'.format(a,b,maiorQ))
+           break
+       else:
+           maiorQ += 1
+
+
